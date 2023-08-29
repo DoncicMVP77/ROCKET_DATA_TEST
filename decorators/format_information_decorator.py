e9e9400ba6c950ea4085964458a585174ca8273f
Daniil Zuev <daniilzuev216@gmail.com>
2023-08-29 09:31:13 +0300

    refactoring decorators

--- decorators/format_information_decorator.py
+++ decorators/format_information_decorator.py
@@ -1,4 +1,5 @@
 import functools
+import re
 
 
 def format_phone_numbers_decorator(func):
@@ -27,3 +28,33 @@ def format_dentalia_hospital_name_decorator(func):
 
     return wrapper
 
+
+def format_hospital_latlon_decorator(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        dict_hospital_latlon = func(*args, **kwargs)
+
+        list_hospital_latlon = [
+            float(dict_hospital_latlon['lat']),
+            float(dict_hospital_latlon['lng'])
+        ]
+
+        return list_hospital_latlon
+
+    return wrapper
+
+
+def format_hospital_working_hours_decorator(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        string_hospital_working_hours = func(*args, **kwargs)
+
+        formatted_string_hospital_working_hours = re.sub(
+            "^\s+|\n|\s+$",
+            " ",
+            re.sub("^\s+|\r|\s+$", "", string_hospital_working_hours)
+        )
+
+        return formatted_string_hospital_working_hours
+
+    return wrapper
